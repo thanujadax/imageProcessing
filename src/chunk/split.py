af299a33775dfb9732b6aae885f45ca7dc16b8b9
Thanuja Ambegoda <thanujadax@gmail.com>
2016-02-12 17:38:56 +0100

    init

--- src/chunk/split.py
+++ src/chunk/split.py
@@ -5,18 +5,19 @@ Created on Feb 12, 2016
 '''
 
 from scipy import ndimage
-from numpy import tile
 
-def simpleSplit(inputImageMat,outputSavePath,maxTileR,maxTileC,paddingPix):
+def simpleSplit(inputImageMat,outputSavePath,tileW,tileH,paddingPix):
     
     '''
     Input parameters:
     inputImageMat - input image as a matrix (numpy 2D array)
-    maxTileR - max number of rows per tile (#pix)
-    maxTileC - max number of columns per tile (#pix)
+    tileW - tile width (#pix)
+    tileH - tile height (#pix)
     paddingPix - number of pixels added to the border from the adjoining tile
     
     Output:
     output tiles are saved in the outputSavePath with file names inputFileName_tile_r000001_c000001.png
     
     '''
+    
+    
