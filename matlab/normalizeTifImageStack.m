9629e0104618474851828fb347dbbb34685b111b
Thanuja Ambegoda <thanujadax@gmail.com>
2016-03-07 21:33:53 +0100

    started im normalization fn

--- matlab/normalizeTifImageStack.m
+++ matlab/normalizeTifImageStack.m
@@ -1,3 +1,8 @@
 function normalizeTifImageStack(inputTifFileName,outputTifFileName)
 % read tiff stack
 % normalize by making mean = 0 and SD = 1
+% uses matlab functions from 'common' directory
+
+% read image stack into array
+% for each image (independently of others), center pixel intensities around
+% zero
