682a542b07045724dc3d0ce8eceb2588af376bb9
Thanuja Ambegoda <thanujadax@gmail.com>
2016-04-03 09:34:44 +0200

    added testSplit.py

---
+++ src/testSplit.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 22, 2016
+to test the simpleSplit function
+@author: thanuja
+'''
+from chunk import Tile
+
+inputFileName = '/home/thanuja/projects/data/drosophilaLarva_ssTEM/isbiSegmentations/raw/test-volume0000.tif'
+outputDirectory = '/home/thanuja/projects/RESULTS/imageSlicer/20160401'
+numberOfTiles = 4
+save = True
+prefix = 't'
+imgFormat = 'png'
+
+Tile.slice(inputFileName, numberOfTiles, save, prefix, outputDirectory, imgFormat)
+
+
+if __name__ == '__main__':
+    pass
